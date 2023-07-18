a6fd44efa0ac325e72b9a12555f21bdc6b471baf
MG5737 <maheshghodekar2001@gmail.com>
2023-07-18 10:24:33 +0530

    feature added

--- SDMTest/src/com/app/Hello.java
+++ SDMTest/src/com/app/Hello.java
@@ -2,9 +2,12 @@ package com.app;
 
 public class Hello {
 
-	
+	public static void mahesh() {
+		System.out.println("Mahesh");
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		Hello.mahesh();
 	}
 }
